d3277e4017fc9b754851bef63c4e346a0d0739f3
Gaurav Mishra <gauravmishra@Apples-MacBook-Pro.local>
2020-02-24 19:25:33 +0530

    write test cases

--- tests/routes/users/test.js
+++ tests/routes/users/test.js
@@ -6,9 +6,37 @@ describe('as', () => {
         mockDB();
     });
 
-    it('should return 200', async () => {
+    it('should return 200 GET', async () => {
         const res = await server.inject({
             method: 'GET',
+            url: '/users/1'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200 POST', async () => {
+        const res = await server.inject({
+            method: 'POST',
+            url: '/users'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200 DELETE', async () => {
+        const res = await server.inject({
+            method: 'DELETE',
+            url: '/users/1'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200 PUT', async () => {
+        const res = await server.inject({
+            method: 'PUT',
+            url: '/users/1'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200 PATCH', async () => {
+        const res = await server.inject({
+            method: 'PATCH',
             url: '/users/1'
         });
         expect(res.statusCode).toEqual(200);

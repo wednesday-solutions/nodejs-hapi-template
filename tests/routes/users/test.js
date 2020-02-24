74681060f6e189dc003f79a9fe94cc342dba187a
Devendra Soni <devendrasoni@Apples-MacBook-Pro.local>
2020-02-24 19:51:22 +0530

    add test cases

--- tests/routes/users/test.js
+++ tests/routes/users/test.js
@@ -13,4 +13,33 @@ describe('as', () => {
         });
         expect(res.statusCode).toEqual(200);
     });
+
+    it('should return 200', async () => {
+        const res = await server.inject({
+            method: 'POST',
+            url: '/users'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200', async () => {
+        const res = await server.inject({
+            method: 'DELETE',
+            url: '/users/1'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200', async () => {
+        const res = await server.inject({
+            method: 'PUT',
+            url: '/users/1'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return 200', async () => {
+        const res = await server.inject({
+            method: 'PATCH',
+            url: '/users/1'
+        });
+        expect(res.statusCode).toEqual(200);
+    });
 });

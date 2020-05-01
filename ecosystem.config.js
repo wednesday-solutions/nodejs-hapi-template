3164b35ed173865856b58f3cf57febc3a26fd846
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-01 14:19:24 +0530

    using port 9000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
             max_memory_restart: '1G',
             env: {
                 NODE_ENV: 'development',
-                PORT: 9001,
+                PORT: 9000,
                 watch: true,
                 DB_URI: databaseConfig.development.url,
                 DB_OPTIONS: JSON.stringify(databaseConfig.development.options)

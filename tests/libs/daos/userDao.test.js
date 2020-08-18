b0400c2344e32ad33800837b1083c166bb0de6e0
christin <christin@wednesday.is>
2020-08-19 00:47:58 +0530

    Add test for findOne parameters Update testUtils mockDB and resetAndMockDB
    Signed-off-by: christin <christin@wednesday.is>

--- tests/libs/daos/userDao.test.js
+++ tests/libs/daos/userDao.test.js
@@ -1,3 +1,5 @@
+import { resetAndMockDB } from 'utils/testUtils';
+
 describe('user daos', () => {
     const mockUser = {
         id: 1,
@@ -13,6 +15,26 @@ describe('user daos', () => {
         expect(testUser.lastName).toEqual(mockUser.lastName);
         expect(testUser.email).toEqual(mockUser.email);
     });
+
+    it('must have findOneUser that finds a user by ID', async () => {
+        let spy;
+        await resetAndMockDB(db => {
+            spy = jest.spyOn(db.users, 'findOne');
+        });
+        const { findOneUser } = require('daos/userDao');
+
+        const userId = 1;
+        await findOneUser(userId);
+        expect(spy).toBeCalled();
+        expect(spy).toBeCalledWith({
+            attributes: ['id', 'first_name', 'last_name', 'email'],
+            underscoredAll: false,
+            where: {
+                id: userId
+            }
+        });
+    });
+
     it('must have findAllUser that finds all the users', async () => {
         const { findAllUser } = require('daos/userDao');
         const { allUsers } = await findAllUser(1, 10);

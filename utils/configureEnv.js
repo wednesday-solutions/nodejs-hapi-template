bc3ac085b81254f35a97da9328d124cba71ebb61
Shounak Mulay <shounak@wednesday.is>
2020-08-18 12:02:50 +0530

    Configure .env path using ENV variable.

--- utils/configureEnv.js
+++ utils/configureEnv.js
@@ -1,9 +1,5 @@
 const dotenv = require('dotenv');
 
 module.exports = () => {
-    if (process.env.ENV === 'production') {
-        dotenv.config();
-    } else {
-        dotenv.config({ path: `.env.${process.env.ENV}` });
-    }
+    dotenv.config({ path: `.env.${process.env.ENV}` });
 };
